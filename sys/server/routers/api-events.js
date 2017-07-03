02c05fcdfd53a874b4b2dab5a04b9959f879066c
Franklin Chieze <chieze.franklin@gmail.com>
2017-07-03 09:59:40 +0100

    Add some comments

--- sys/server/routers/api-events.js
+++ sys/server/routers/api-events.js
@@ -6,10 +6,13 @@ var apiEventsCtrlr = require('../controllers/api-events');
 
 var router = express.Router();
 
+//publishes an event
 router.post('/:name', checksCtrlr.getAppName, apiEventsCtrlr.postEvent);
 
+//subscribes to an event using a transient hook
 router.post('/sub/*', checksCtrlr.getAppName, apiEventsCtrlr.postSub);
 
+//deletes a subscription to an event that was made using a transient hook
 router.delete('/sub/*', checksCtrlr.getAppName, apiEventsCtrlr.deleteSub);
 
 //TODO: an endpoint for validating events
