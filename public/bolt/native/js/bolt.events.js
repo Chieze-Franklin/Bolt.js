3fff8d54fe1585d44b08368283465906d7d957d1
Franklin Chieze <chieze.franklin@gmail.com>
2017-07-08 22:42:19 +0100

    Rename events.js to bolt.events.js

---
+++ public/bolt/native/js/bolt.events.js
@@ -0,0 +1,54 @@
+var Bolt = (function(bolt){
+
+	hooks = [];
+
+	bolt.Events = {
+		emit: function(eventName, eventData, appToken, headers){
+			//
+		},
+		on: function(route, handler, name){
+			route = route.replace("\\", "/");
+
+			var publisher, evnt;
+
+			if (route.indexOf("/") == -1) {
+				publisher = "*";
+				evnt = route;
+			}
+			else {
+				publisher = route.substring(0, route.indexOf("/"));
+				if (publisher == "") publisher = "*";
+
+				evnt = route.substr(route.indexOf("/") + 1);
+				if (evnt == "") evnt = "*";
+			}
+
+			var newHook = {
+				event: evnt,
+				publisher: publisher,
+				handler: handler
+			};
+			if (name) {
+				newHook.name = name;
+			}
+			hooks.push(newHook)
+		},
+		remove: function(name){
+			//
+		}
+	};
+
+	var socket = io.connect("/");
+
+	socket.on('message', function (event) {
+		event = JSON.parse(event);
+		hooks.forEach(function(hook){
+			if ((hook.event == event.name || hook.event == "*") 
+				&& (hook.publisher == event.publisher || hook.publisher == "*")) {
+				if (typeof hook.handler === "function") hook.handler(event);
+			}
+		});
+	});
+
+	return bolt;
+}(Bolt || {}));
