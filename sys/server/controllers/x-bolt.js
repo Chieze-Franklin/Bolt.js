be9e8c263f9d95b5283720b25d106e6798f2e406
Franklin Chieze <chieze.franklin@gmail.com>
2017-07-11 13:27:39 +0100

    Create request.bolt object in '/x/bolt/*'

--- sys/server/controllers/x-bolt.js
+++ sys/server/controllers/x-bolt.js
@@ -10,25 +10,25 @@ module.exports = {
 	postHooksBoltAppDeleted: function(request, response) {
 		var event = request.body;
 
-		if (event.token == request.appToken) {
+		if (event.token == request.bolt.token) {
 			var app = event.body;
 
 			//delete app-roles
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/app-roles?app=" + app.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 
 			//delete app-users
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/app-users?app=" + app.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 
 			/*//delete collections
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/db")
-				.set(X_BOLT_APP_TOKEN, request.genAppToken(app.name))
+				.set(X_BOLT_APP_TOKEN, request.bolt.genAppToken(app.name))
 				.send({app: app.name})
 				.end(function(err, res){});*/
 			//-----------------------------
@@ -41,7 +41,7 @@ module.exports = {
 	postHooksBoltAppRouterLoaded: function(request, response) {
 		var event = request.body;
 
-		if (event.token == request.appToken) {
+		if (event.token == request.bolt.token) {
 			var rtr = event.body;
 			console.log("Loaded router%s%s%s",
 		            (!utils.Misc.isNullOrUndefined(rtr.name)
@@ -61,7 +61,7 @@ module.exports = {
 		//TODO: create app-user object, or increase starts by 1 if it already exists
 		var event = request.body;
 
-		if (event.token == request.appToken) {
+		if (event.token == request.bolt.token) {
 			var context = event.body;
 			var user = request.user;
 
@@ -71,7 +71,7 @@ module.exports = {
 						//create app-user object
 						superagent
 							.post(process.env.BOLT_ADDRESS + "/api/app-users")
-							.set(X_BOLT_APP_TOKEN, request.appToken) //TODO: check request.appToken for foreign requests
+							.set(X_BOLT_APP_TOKEN, request.bolt.token) //TODO: check request.bolt.token for foreign requests
 							.send({ app: context.name, user: user.name, starts: 1 })
 							.end(function(err, res){});
 					}
@@ -91,19 +91,19 @@ module.exports = {
 		//when a role is deleted, delete relevant app-roles and user-roles
 		var event = request.body;
 
-		if (event.token == request.appToken) {
+		if (event.token == request.bolt.token) {
 			var role = event.body;
 
 			//delete app-roles
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/app-roles?role=" + role.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 
 			//delete user-roles
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/user-roles?role=" + role.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 		}
 
@@ -113,19 +113,19 @@ module.exports = {
 		//when a user is deleted, delete relevant user-roles and app-users
 		var event = request.body;
 
-		if (event.token == request.appToken) {
+		if (event.token == request.bolt.token) {
 			var user = event.body;
 
 			//delete app-users
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/app-users?user=" + user.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 
 			//delete user-roles
 			superagent
 				.delete(process.env.BOLT_ADDRESS + "/api/user-roles?user=" + user.name)
-				.set(X_BOLT_APP_TOKEN, request.appToken)
+				.set(X_BOLT_APP_TOKEN, request.bolt.token)
 				.end(function(err, res){});
 		}
 
