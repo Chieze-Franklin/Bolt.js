34d8b21b024ff510e327cad2d8b7dfcc44414523
Franklin Chieze <chieze.franklin@gmail.com>
2017-09-09 22:21:24 +0100

    Added more methods to Bolt.Env

--- public/bolt/native/js/bolt.env.js
+++ public/bolt/native/js/bolt.env.js
@@ -1,12 +1,31 @@
 var Bolt = (function(bolt){
 	var __address;
+	var __protocol, __host, __port;
 
 	bolt.Env = {
 		getAddress: function(){
 			return __address;
 		},
-		setAddress: function(port){
-			__address = port;
+		setAddress: function(address){
+			__address = address;
+		},
+		getHost: function(){
+			return __host;
+		},
+		setHost: function(host){
+			__host = host;
+		},
+		getPort: function(){
+			return __port;
+		},
+		setPort: function(port){
+			__port = port;
+		},
+		getProtocol: function(){
+			return __protocol;
+		},
+		setProtocol: function(protocol){
+			__protocol = protocol;
 		}
 	};
 
